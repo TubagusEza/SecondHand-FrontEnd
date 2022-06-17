d35077215fb97d5d04a6699820c85f741793c865
Safira Tyas Wandita <safeeratyas@gmail.com>
2022-06-17 14:24:12 +0700

    feat:add carousel, card, and modal for product page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import './styles/App.css';
+// import HomeNavbar from './components/Navbar/HomeNavbar'
+import SellerProductPage from './components/Seller/Carousel/Carousel'
+import BuyerProductPage from './components/Buyer/Carousel/Carousel'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <BrowserRouter>
+       <Routes>
+       <Route path='/seller/product/:id' element={<SellerProductPage />} />
+       <Route path='/buyer/product/:id' element={<BuyerProductPage />} />
+       </Routes>
+      </BrowserRouter>
+  )
 }
 
 export default App;
