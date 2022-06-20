f6ec4fb28bd379e89a9f6c18e1ce6008e1879d20
Ghifari Astaudi <ghifariastaudi@gmail.com>
2022-06-20 13:52:36 +0700

    push navbar

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Homepage from "./pages/Homepage";
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
